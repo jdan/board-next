5831aedf210ce70eba28564ec019c33578e561f5
Jordan Scales <scalesjordan@gmail.com>
2021-08-07 15:44:39 -0400

    padding and add link to img

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,28 @@
+import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
   const [pgn, setPgn] = useState("");
   return (
     <>
-      <div>
+      <div style={{ padding: 20 }}>
         <label>
           PGN: <input onChange={(e) => setPgn(e.target.value)} value={pgn} />
         </label>
       </div>
 
-      <img
-        width={500}
-        height={500}
-        alt={pgn}
-        src={`/api/board.svg?pgn=${encodeURIComponent(pgn)}`}
-      />
+      <div style={{ padding: 20 }}>
+        <a href={`/api/board.svg?pgn=${encodeURIComponent(pgn)}`}>link</a>
+      </div>
+
+      <div style={{ padding: 20 }}>
+        <Image
+          width={500}
+          height={500}
+          alt={pgn}
+          src={`/api/board.svg?pgn=${encodeURIComponent(pgn)}`}
+        />
+      </div>
     </>
   );
 }
